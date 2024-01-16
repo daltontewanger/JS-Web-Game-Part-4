8e5ad480f0247ace41c18a2a0acfc799d58fee72
Dalton Tewanger <daltontewanger@yahoo.com>
2024-01-15 19:53:29 -0800

    Placed the move character function to move.js and updated index.js with a handle direction function.

--- index.js
+++ index.js
@@ -2,52 +2,26 @@ const inventory = newInventory();
 move(inventory).to(0, 0);
 
 const character = newImage("assets/green-character/static.gif");
-let direction = null;
-let x = 100;
-let y = 250;
 
-function moveCharacter() {
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
   if (direction === "up") {
-    y = y + 1;
+    character.src = "assets/green-character/north.gif";
   }
   if (direction === "down") {
-    y = y - 1;
+    character.src = "assets/green-character/south.gif";
   }
   if (direction === "left") {
-    x = x - 1;
+    character.src = "assets/green-character/west.gif";
   }
   if (direction === "right") {
-    x = x + 1;
+    character.src = "assets/green-character/east.gif";
   }
-
-  character.style.left = x + "px";
-  character.style.bottom = y + "px";
 }
 
-setInterval(moveCharacter, 1);
-
-document.addEventListener("keydown", function (e) {
-  if (e.repeat) return;
-
-  if (e.key === "ArrowUp") {
-    direction = "up";
-  }
-  if (e.key === "ArrowDown") {
-    direction = "down";
-  }
-  if (e.key === "ArrowLeft") {
-    direction = "left";
-  }
-  if (e.key === "ArrowRight") {
-    direction = "right";
-  }
-});
-
-document.addEventListener("keyup", function (e) {
-  direction = null;
-});
-
-move(character).to(100, 250);
+move(character).withArrowKeys(100, 250, handleDirectionChange);
 
 move(newImage("assets/tree.png")).to(200, 450);
 move(newImage("assets/pillar.png")).to(350, 250);
--- move.js
+++ move.js
@@ -1,12 +1,65 @@
 function move(element) {
-    element.style.position = 'fixed'
+  element.style.position = "fixed";
 
-    function moveToCoordinates(left, bottom) {
-        element.style.left = left + 'px'
-        element.style.bottom = bottom + 'px'
+  function moveToCoordinates(left, bottom) {
+    element.style.left = left + "px";
+    element.style.bottom = bottom + "px";
+  }
+
+  function moveWithArrowKeys(left, bottom, onDirectionChange) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + "px";
+    element.style.bottom = y + "px";
+
+    function moveCharacter() {
+      if (direction === "up") {
+        y = y + 1;
+      }
+      if (direction === "down") {
+        y = y - 1;
+      }
+      if (direction === "left") {
+        x = x - 1;
+      }
+      if (direction === "right") {
+        x = x + 1;
+      }
+
+      element.style.left = x + "px";
+      element.style.bottom = y + "px";
     }
 
-    return {
-        to: moveToCoordinates
-    }
+    setInterval(moveCharacter, 1);
+
+    document.addEventListener("keydown", function (e) {
+      if (e.repeat) return;
+
+      if (e.key === "ArrowUp") {
+        direction = "up";
+      }
+      if (e.key === "ArrowDown") {
+        direction = "down";
+      }
+      if (e.key === "ArrowLeft") {
+        direction = "left";
+      }
+      if (e.key === "ArrowRight") {
+        direction = "right";
+      }
+      onDirectionChange(direction);
+    });
+
+    document.addEventListener("keyup", function (e) {
+      direction = null;
+      onDirectionChange(direction);
+    });
+  }
+
+  return {
+    to: moveToCoordinates,
+    withArrowKeys: moveWithArrowKeys,
+  };
 }
